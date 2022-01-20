896be5867ede08080fa357235f17a7fb5fbd436d
admin <admin@admin.com>
2022-01-20 17:23:52 +0800

    111

---
+++ src/main/java/com/test.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author shkstart
+ * @create 2022-01-20 17:22
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
